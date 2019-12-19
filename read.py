b437b5ee72b33268b577b8972f133ba3cf0acf86
Lazaro Carroll <lazarocarroll@gmail.com>
2019-12-18 22:06:32 -0500

    updated algorythm

--- read.py
+++ read.py
@@ -6,12 +6,32 @@ from pandas import ExcelFile
 df = pd.read_csv('a.csv')
 data_f = pd.DataFrame(data=df)
 print(data_f)
-x = 1
-for x in len(data_f.values):
-    print(X)
-    x+1
+x = 0
+MALE = []
+FEMALE = []
+BOTH = []
 
-#print(data_f[0])
+
+#parsing while loop to sort 3 arrays (Male, Female, Both)
+for x in range(int(len(data_f.values))-1):
+    if data_f.values[x,5] == "M":
+        MALE.append(x)                
+#        x+=1
+    elif data_f.values[x,5] == "F":
+        FEMALE.append(x)
+#        x+=1
+    elif data_f.values[x,5] == "B":
+        BOTH.append(x)
+#        x+=1
+    else:
+        print(data_f.values[x,1], "does not have a correct Gender preference or format please review\n this is what is in the prefence field:", print(data_f.values[x,3]))
+    print(x)
+    x+=1
+#    print(data_f.values[x,1], " wants to call", data_f.values[x,3])
+
+
+print("BEHOLD THE DATA!", MALE, FEMALE, BOTH)
+#print(data_f.values[0])
 
 #print("colum headings:")
 #print(pd.DataFrame.equals(df))
