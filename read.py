6971b5900e30ee199d5ba06f3ee922a67c27c959
Lazaro Carroll <lazarocarroll@gmail.com>
2019-12-18 22:57:23 -0500

    Random implamented

--- read.py
+++ read.py
@@ -1,3 +1,4 @@
+import random
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
@@ -15,22 +16,38 @@ BOTH = []
 #parsing while loop to sort 3 arrays (Male, Female, Both)
 for x in range(int(len(data_f.values))-1):
     if data_f.values[x,5] == "M":
-        MALE.append(x)                
+        MALE.append(x)
 #        x+=1
     elif data_f.values[x,5] == "F":
         FEMALE.append(x)
 #        x+=1
     elif data_f.values[x,5] == "B":
         BOTH.append(x)
-#        x+=1
     else:
         print(data_f.values[x,1], "does not have a correct Gender preference or format please review\n this is what is in the prefence field:", print(data_f.values[x,3]))
     print(x)
     x+=1
 #    print(data_f.values[x,1], " wants to call", data_f.values[x,3])
 
+class rand_sort:
+    def __init(self, array_index):
+        self.array_index = array_index
+    def rand_num(self):
+        for x in len(self.array_index):
+            print(array_index[x])
 
-print("BEHOLD THE DATA!", MALE, FEMALE, BOTH)
+
+print("BEHOLD THE DATA!",MALE, FEMALE, BOTH)
+print("Here is a Random Values from the MALE Group", random.choice (MALE))
+print("Here is a Random Values from the FEMALE Group", random.choice (FEMALE))
+print("Here is a Random Values from the BOTH Group", random.choice (BOTH))
+
+for y in range(len(MALE)):
+    for x in range(data_f.values[MALE[y], 4]):
+        print(data_f.values[y,1], "Will call", data_f.values[random.choice (MALE), 1])
+    
+    
+
 #print(data_f.values[0])
 
 #print("colum headings:")
