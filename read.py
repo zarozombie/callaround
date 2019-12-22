935c4b1dc63fad4e2367446df1bfed24679bc5b1
Lazaro Carroll <lazarocarroll@gmail.com>
2019-12-22 11:52:53 -0500

    orginized code

--- read.py
+++ read.py
@@ -3,32 +3,32 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
-#df = pd.read_excel('a.csv' , sheetname='Sheet1')
 df = pd.read_csv('a.csv')
 data_f = pd.DataFrame(data=df)
-#print(data_f)
 x = 0
 MALE = []
 FEMALE = []
 BOTH = []
 
+#print all data in table
+#print(data_f)
 
-#parsing while loop to sort 3 arrays (Male, Female, Both)
+
+#print("welcome to the call around program")
+#print("press ENTER to start")dd
+#parsing while loop to sort 3 arrays (group by preference Male, Female, Both)i
+#possibally append to matrix array
 for x in range(int(len(data_f.values))-1):
     if data_f.values[x,5] == "M":
         MALE.append(x)
-#        x+=1
     elif data_f.values[x,5] == "F":
         FEMALE.append(x)
-#        x+=1
     elif data_f.values[x,5] == "B":
         BOTH.append(x)
     else:
         print(data_f.values[x,1], "does not have a correct Gender preference or format please review\n this is what is in the prefence field:", print(data_f.values[x,3]))
-#    print(x)
-    x+=1
-#    print(data_f.values[x,1], " wants to call", data_f.values[x,3])
 
+#test Class not yet implamented
 class rand_sort:
     def __init(self, array_index):
         self.array_index = array_index
@@ -37,11 +37,19 @@ class rand_sort:
             print(array_index[x])
 
 
+#output Options#####################################################################
+
+#print output of all arrays
 #print("BEHOLD THE DATA!",MALE, FEMALE, BOTH)
+
+#print randon choices
 #print("Here is a Random Values from the MALE Group", random.choice (MALE))
 #print("Here is a Random Values from the FEMALE Group", random.choice (FEMALE))
 #print("Here is a Random Values from the BOTH Group", random.choice (BOTH))
 
+####################################################################################
+
+#output Template for all random results
 print("---------------------------\n\n")
 
 for y in range(len(MALE)):
@@ -66,10 +74,13 @@ for y in range(len(BOTH)):
     for x in range(data_f.values[BOTH[y], 4]):
         print(data_f.values[BOTH[y],1], "Will call", data_f.values[random.choice (BOTH), 1])
 
-#print(data_f.values[0])
+#########################################################################################################
+#Need method of sending data to twillio##################################################################
+#########################################################################################################
 
+
+#Other Output Options ####################################
+#print(data_f.values[0])
 #print("colum headings:")
 #print(pd.DataFrame.equals(df))
-
-#print("welcome to the call around program")
-#print("press ENTER to start")
+#############################################################
